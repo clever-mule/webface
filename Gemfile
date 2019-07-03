9d24a68aa5d1e2fc8dcb08bd1f688d41eb0b472a
Artemiy Solopov <art-solopov@yandex.ru>
2019-07-03 21:57:13 +0300

    Added the dataset display

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'sinatra', require: 'sinatra/base'
+gem 'slim'
+
 group :development do
   gem 'rubocop'
 
--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
+
+require 'csv'
+
 require 'bundler/setup'
 Bundler.require(:default, :development)
 
 DATASET_ATTRIBUTION = 'https://data.deutschebahn.com/dataset/data-stationsdaten'
+DATASET_PATH = File.join(File.dirname(File.absolute_path(__FILE__)),
+                         '..',
+                         'DBSuS-Uebersicht_Bahnhoefe-Stand2019-03.csv')
+
+module DataReader
+  def self.call
+    CSV.foreach(DATASET_PATH, col_sep: ';', headers: true).take(100)
+  end
+end
+
+class WebfaceApp < Sinatra::Application
+  get '/' do
+    @title = 'Dataset'
+    @data = DataReader.call
+    slim :home
+  end
+end
